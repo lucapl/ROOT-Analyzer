6beb64037f788804fff598c0fead61cd2033f528
lucapl <lukasz.andryszewski@outlook.com>
2024-01-02 01:06:57 +0100

    Added function to saturate images

--- src/utils.py
+++ src/utils.py
@@ -27,3 +27,13 @@ def crop_contour(img,contour):
     x,y,w,h = cv2.boundingRect(contour)
     cropped = img[y:y+h,x:x+w]
     return cropped
+
+def saturate_image(img,saturation=2):
+    img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV).astype("float32")
+    
+    (h, s, v) = cv2.split(img_hsv)
+    s = s*saturation
+    s = np.clip(s,0,255)
+    img_hsv = cv2.merge([h,s,v])
+    
+    return cv2.cvtColor(img_hsv.astype("uint8"), cv2.COLOR_HSV2BGR)
