2f41d49d71d656493606bd8aee9823f1be58c280
lucapl <lukasz.andryszewski@outlook.com>
2024-01-09 18:37:55 +0100

    Modularized standalone programs

--- src/getFirstFrame.py
+++ src/getFirstFrame.py
@@ -1,15 +1,16 @@
 import cv2
 from sys import argv
 
-def getFirstFrame(_input,_output="first_frame.png"):
-    print(_input,_output)
+def getFirstFrame(_input,verbose=False):
+    if verbose: print(_input,_output)
     vidcap = cv2.VideoCapture(_input)
     if vidcap.isOpened():
-        print("Video loaded")
+        if verbose: print("Video loaded")
     success, image = vidcap.read()
     if success:
-        print(_output)
-        cv2.imwrite(_output, image)
+        return image
 
 if __name__ == '__main__':
-    getFirstFrame(*argv[1:])
+    image = getFirstFrame(*argv[1:])
+    if image != None:
+        cv2.imwrite("first_frame.png",image)
