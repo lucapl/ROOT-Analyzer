be3b7ccbe6d8b59aa46d48d6f60e9744340ccfe3
lucapl <lukasz.andryszewski@outlook.com>
2024-01-09 18:35:57 +0100

    Visualizations module

---
+++ src/viz/graphs.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+
+def draw_map(board_info):
+    G_map = nx.grid_2d_graph(4,3)
+    G_map.remove_edges_from(list(G_map.edges()))
+
+    i_to_x_y = lambda i: (i%4,2-(i//4))
+
+    labels = {i_to_x_y(i):i for i in range(12)}
+
+    for i,connections in enumerate(board_info["connections"]):
+        x,y = i_to_x_y(i)
+        for j in connections:
+            z,w = i_to_x_y(j)
+            G_map.add_edge((x,y),(z,w))
+
+
+    pos = dict( (n, n) for n in G_map.nodes() )
+    nx.draw_networkx(G_map,pos=pos, labels=labels)
+    plt.axis('off')
+    plt.show()    
---
+++ src/viz/images.py
@@ -0,0 +1,23 @@
+import cv2
+import matplotlib.pyplot as plt
+import numpy as np
+import PIL
+import pandas as pd
+from IPython.display import display, HTML
+
+
+def imshow(img):
+    ''' imshow function courtesy of our lab notebooks '''
+
+    img = img.clip(0, 255).astype("uint8")
+    if img.ndim == 3:
+        if img.shape[2] == 4:
+            img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGBA)
+        else:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    display(PIL.Image.fromarray(img))
+
+def scaled_imshow(img,fx=0.3,fy=0.3):
+    ''' scaling the image so the notebook doesn't take too much space '''
+    scaled_img = cv2.resize(img,None,fx=fx,fy=fy)
+    imshow(scaled_img)
